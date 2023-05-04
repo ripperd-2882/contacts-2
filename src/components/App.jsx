e4b0b8865e311229987f349c8b1e52dfdaa13389
ripperd-2882 <ripperd2882@gmail.com>
2023-05-05 01:58:52 +0530

    Mapped components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,17 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+    return <Card
+        key={contact.id}
+        id={contact.id}
+        name={contact.name}
+        img={contact.imgURL}
+        tel={contact.phone}
+        email={contact.email}
+    />
+}
+
 function App() {
     return (
         <div>
@@ -11,8 +22,9 @@ function App() {
             <Avatar
                 img="https://flxt.tmsimg.com/assets/238324_v9_bb.jpg"
             />
+            {contacts.map(createCard)}
 
-            <Card
+            {/* <Card
                 name={contacts[0].name}
                 img={contacts[0].imgURL}
                 tel={contacts[0].phone}
@@ -29,7 +41,7 @@ function App() {
                 img={contacts[2].imgURL}
                 tel={contacts[2].phone}
                 email={contacts[2].email}
-            />
+            /> */}
         </div>
     );
 }
